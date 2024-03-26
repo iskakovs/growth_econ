722e94fadaee7bac9a64447ef12b0862fb32feec
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2024-03-27 01:46:51 +0500

    Update france.R

--- france.R
+++ france.R
@@ -9,3 +9,13 @@ library(openxlsx)
 
 # Read the dataset from the Excel file
 data <- read_excel('...\\fr-world.xlsx', sheet = 'data')
+
+# 1. Plotting
+ggplot(data, aes(x = Year)) + 
+  geom_line(aes(y = World, color = "Мир")) + # Plot World GDP growth
+  geom_line(aes(y = France, color = "Франция")) + # Plot France GDP growth
+  labs(title = "Динамика темпов экономического роста",
+       y = "Темп роста ВВП (%)",
+       x = "Год") +
+  scale_color_manual(name = "Регион", values = c("Мир" = "blue", "Франция" = "red")) +
+  theme_minimal() # Using a minimal theme for the plot
